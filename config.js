d1d8045797485647d785a5df510ff4489a6f2e92
ttionya <git@ttionya.com>
2017-03-05 21:44:29 +0800

    Just test

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+
+    // 待爬取的 URL，变量用 {{SUDOKU}} 替代
+    "url": "http://oubk.com/sudoku/{{SUDOKU}}.html",
+
+
+}
--- index.js
+++ index.js
@@ -2,3 +2,40 @@ import "babel-polyfill";
 import superAgent from 'superagent';
 import cheerio from 'cheerio';
 import { mapLimit } from 'async';
+import config from './config';
+
+
+
+let array = [];
+for (let i = 1; i <= 2000000; i++) {
+    array.push(i);
+}
+
+mapLimit(array, 2, function (id, callback) {
+    superAgent.get(config.url.replace('{{SUDOKU}}', id.toString()))
+        .end((err, res) => {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                var $ = cheerio.load(res.text);
+                let sudoku = [];
+
+                for (let i = 1; i < 10; i++) {
+                    for (let j = 1; j < 10; j++) {
+                        sudoku.push($('#k' + j + 's' + i).val());
+                    }
+                }
+
+                console.log($('#hid_aw').val());
+                console.log(sudoku.join());
+            }
+
+            if (id > 10) {
+                callback(1);
+            }
+            else {
+                callback(null);
+            }
+        });
+});
